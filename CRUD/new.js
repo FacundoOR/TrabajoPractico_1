92e321879dad25af97f070169604b9aed80233de
Facundo Rosas <facurosas7@gmail.com>
2022-08-10 10:15:23 -0300

    mejora para que no se repitan los id's

--- CRUD/new.js
+++ CRUD/new.js
@@ -1,10 +1,11 @@
 const read = require('./read')
 const writeF = require('./writeJson.js')
 
-let new_book = (title, author, genre, year, cost, price ) => {
-     new_array = read() 
-     id_new = new_array.length 
-    new_array.push({ id: id_new, title: title, author: author, genre: genre, year: year, cost: cost, price: price})
+let new_book = (title, author, genre, year, cost, price) => {
+    new_array = read()
+    id_new = new_array.length
+    new_array.map((elemento) => { if (elemento.id == id_new) id_new = id_new + 1 })
+    new_array.push({ id: id_new, title: title, author: author, genre: genre, year: year, cost: cost, price: price })
     writeF.write(new_array)
 }
 
